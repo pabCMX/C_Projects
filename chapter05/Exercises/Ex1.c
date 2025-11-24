bfc959fa3ef30ae391f02cc131f2c2f21596208b
CMX <53591196+pabCMX@users.noreply.github.com>
2025-11-24 00:40:06 -0500

    Started on Chapter 5 programming projects, finished 1 and 2.

---
+++ chapter05/Exercises/Ex1.c
@@ -0,0 +1,24 @@
+// Calculates digits in a given number.
+#include <stdio.h>
+
+int main(void) {
+  int userNum;
+  int count;
+
+  printf("Enter a number: ");
+  scanf("%d", &userNum);
+
+  if (userNum <= 9) {
+    count = 1;
+  } else if (userNum <= 99) {
+    count = 2;
+  } else if (userNum <= 999) {
+    count = 3;
+  } else if (userNum <= 9999) {
+    count = 4;
+  } else {
+    count = 5;
+  }
+
+  printf("The number %d has %d digits.", userNum, count);
+}
---
+++ chapter05/Exercises/Ex2.c
@@ -0,0 +1,26 @@
+// Converts 24 hour time to 12 hour time
+// Pitfalls include not managing am/pm for 12 pm and 12 am, as well as missing digits on minutes.
+#include <stdio.h>
+
+int main(void) {
+  int userHours, userMinutes, convertedHours;
+
+
+  printf("Enter a 24-hour time (HH:MM): ");
+  scanf("%2d:%2d", &userHours, &userMinutes);
+
+  if (userHours >= 13) {
+    convertedHours = userHours - 12;
+    printf("Equivalent 12-hour time: %.2d:%.2d PM", convertedHours, userMinutes);
+  } else if (userHours == 12) {
+    convertedHours = userHours;
+    printf("Equivalent 12-hour time: %.2d:%.2d PM", convertedHours, userMinutes);
+  } else if (userHours > 0) {
+    convertedHours = userHours;
+    printf("Equivalent 12-hour time: %.2d:%.2d AM", convertedHours, userMinutes);
+  } else {
+    convertedHours = 12;
+    printf("Equivalent 12-hour time: %.2d:%.2d AM", convertedHours, userMinutes);
+  }
+
+}
